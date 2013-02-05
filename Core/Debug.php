6a470fb9d84ad9ed333fee05516d533367df719f
Sergey Kukunin <tamplier0608@gmail.com>
2013-02-05 22:01:15 +0200

    + add new files

---
+++ Core/Debug.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Custom debug with logging data
+ *
+ * @package Core
+ * @subpackage Debug
+ * @author Sergey Kukunin <tamplier0608@gmail.com>
+ * @version 0.1
+ * @license GPL
+ *
+ * @since 1.0
+ *
+ * Date: 18.01.13
+ * Time: 22:45
+ *
+ */
+
+class Core_Debug extends Core_Debug_DebugAbstract
+{
+
+}
